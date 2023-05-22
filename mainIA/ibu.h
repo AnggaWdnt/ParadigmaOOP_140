a54b8d2ce2558220a84662155ab5fc23e779aae9
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2023-05-22 18:55:52 +0700

    pembuatan header ibu

---
+++ mainIA/ibu.h
@@ -0,0 +1,18 @@
+#ifndef IBU_H
+#define IBU_H
+
+class ibu {
+public:
+	string nama;
+	vector<anak*> daftar_anak;
+
+	ibu(string pNama) :nama(pNama) {
+		cout << "Ibu \"" << nama << "\" tidak ada\n";
+	}
+	~ibu() {
+		cout << "Ibu" \"" << nama << "\" tidak ada\n";
+	}
+	void tambahanAnak(anak*);
+	void detakAnak();
+};
+#endif
