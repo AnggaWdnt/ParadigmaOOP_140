b863ec59b7f6ded37e96311cf832dacbfd848925
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2023-05-22 18:04:12 +0700

    tambahan void pasien cetak dokter

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -32,3 +32,10 @@ public:
 void pasien::tambahanDokkter(dokter* pDokter) {
 	daftar_dokter.push_back(pDokter);
 }
+void pasien::cetakDokter() {
+	cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
+	for (auto& a : daftar_dokter) {
+		cout << a->nama << "\n";
+	}
+	cout << endl;
+}
